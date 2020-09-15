a74eda06edb36baf42f5e6b6460537821176d0e2
Talita <talita.leite@bandtec.com.br>
2020-09-15 11:15:16 -0300

    Inseri as colunas de data e hora no banco

--- bd/BD_sensor.sql
+++ bd/BD_sensor.sql
@@ -12,7 +12,7 @@ insert into Usuario values
 (null, 'Bruna Brito', '123'),
 (null, 'Yuri Cruz', '987');
 
-select * from Usuario;
+select * from Usuario; 
 
 
 create table Cadastro (
@@ -28,7 +28,7 @@ insert into Cadastro values
 ('Bruna Brito', 'bruna.melo@bandtec.com.br','1998-04-06', '960416628','Rua Porto Lucena','47633986875'),
 ('Yuri Cruz', 'yuri.cruz@bandtec.com.br','2002-06-18','996488522', 'Rua Prof Tadeu', '48753827821');
 
-select * from Cadastro;
+select * from Cadastro; 
 
 create table Sensor (
 idSensor int primary key auto_increment,
@@ -38,7 +38,7 @@ Setor varchar (80) );
 
 alter table Sensor add column dataSensor datetime;
 
-select * from Sensor;
+select * from Sensor; 
 alter table Sensor drop column Setor;
 
 
@@ -46,5 +46,9 @@ insert into Sensor values
 (null, '25', '30','Estufa'),
 (null, '30', '32','Lado Sul'),
 (null, '20', '34','Lado Leste'); 
+
+update Sensor set dataSensor = '2020-08-09 11:06:25' where idSensor = 1;
+update Sensor set dataSensor = '2020-09-15 12:10:45' where idSensor = 2;
+update Sensor set dataSensor = '2020-10-20 13:00:19' where idSensor = 3;
 
 select * from Sensor;
