acbca599e5e97879b9b306a157d78e03d0e16a53
Yuri-Cruz <yuri.cruz@bandtec.com.br>
2020-09-18 11:24:30 -0300

    alterei as cores dos gráficos do api

--- api gráfico/app/sensors.js
+++ api gráfico/app/sensors.js
@@ -1,34 +1,3 @@
-function lm35(min, max) {
-    min = typeof min == 'undefined' ? 18 : min;
-    max = typeof max == 'undefined' ? 25 : max;
-    
-    let random = Math.random() * (max - min) + min;
-
-    return random
-}
-
-function trc5000() {
-    min = 0;
-    max = 1;
-    
-    let random = Math.random() * (max - min) + min;
-    
-    if (random >= 0.7) {
-        return 1
-    } else if (random <= 0.7) {
-        return 0
-    }
-}
-
-function ldr(){
-    min = 0;
-    max = 1023
-
-    let random = Math.floor(Math.random()*(max-min+1)+min);
-
-    return random
-}
-
 function dht11(options){
 
     minHumidity = options.minHum;
@@ -45,8 +14,8 @@ function dht11(options){
         throw new Error('Os valores minímos e máximos para temperatura são 0 e 50 respectivamente.')
     }
 
-    minTemperature = typeof minTemperature == 'undefined' ? 18 : minTemperature;
-    maxTemperature = typeof maxTemperature == 'undefined' ? 25 : maxTemperature;
+    minTemperature = typeof minTemperature == 'undefined' ? 30 : minTemperature;
+    maxTemperature = typeof maxTemperature == 'undefined' ? 40 : maxTemperature;
 
     minHumidity = typeof minHumidity == 'undefined' ? 20 : minHumidity;
     maxHumidity = typeof maxHumidity == 'undefined' ? 80 : maxHumidity;
@@ -59,4 +28,4 @@ function dht11(options){
 }
 
 
-module.exports = {lm35, trc5000, ldr, dht11};
+module.exports = {dht11};
